7130852b071ebaa86fe2aa17e0a748af13ff64c7
Tom <tom.lesellier@univ-lyon2.fr>
2026-03-17 14:03:58 +0100

    Initial Commit

---
+++ SakuraNinja/PauseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PauseViewController.swift
+//  SakuraNinja
+//
+//  Created by Tom Lesellier on 27/02/2026.
+//
+
+import UIKit
+
+class PauseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SakuraNinja/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SakuraNinja
+//
+//  Created by Tom Lesellier on 27/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
