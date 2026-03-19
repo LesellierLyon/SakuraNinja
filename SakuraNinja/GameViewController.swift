ce2589ee56d833672277e168bfaf0f9d59130fdb
Tom <tom.lesellier@univ-lyon2.fr>
2026-03-19 11:22:57 +0100

    push plusieurs fleurs

--- SakuraNinja/GameViewController.swift
+++ SakuraNinja/GameViewController.swift
@@ -16,17 +16,27 @@ class GameViewController: UIViewController {
     var nbVie = 0
     var t: Timer!
     var t2: Timer!
+    var attente : Int = 0
+
     var screenHeight : Int = 0
     var screenWidth : Int = 0
     var positionx : [Int] = []
+    var temps : Int = 0
     
     @IBOutlet weak var boutonpause: UIButton!
     @IBOutlet weak var boutonmenu: UIButton!
     @IBOutlet weak var boutonrejouer: UIButton!
-    var v = CGPoint(x:0, y:0)
-    
+    var vitesses: [UIImageView: CGPoint] = [:]
+
     @IBOutlet weak var finpartie: UILabel!
-    @IBOutlet weak var Fleur: UIImageView!
+    
+    @IBOutlet weak var fleur1: UIImageView!
+    @IBOutlet weak var fleur2: UIImageView!
+    @IBOutlet weak var fleur3: UIImageView!
+    @IBOutlet weak var fleur4: UIImageView!
+    var fleurs : [UIImageView] = []
+    let fleur = UIImageView()
+
     
     @IBAction func rejouer(_ sender: Any) {
         nbVie=0
@@ -37,64 +47,96 @@ class GameViewController: UIViewController {
         boutonmenu.isHidden=true
         boutonrejouer.isHidden=true
         boutonpause.isHidden=false
+        fleur1.isHidden=true
+        fleur2.isHidden=true
+        fleur3.isHidden=true
+        fleur4.isHidden=true
+        print(fleur1.frame.origin)
         viewDidLoad()
     }
-    @objc func deplace(t2: Timer) {
-        Fleur.frame.origin = CGPoint(x: positionx[Int.random(in: 0...1)], y: Int.random(in:100...screenHeight))
-        t = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(boucle), userInfo: nil, repeats: true)
-        if Fleur.frame.origin.x < 0 {
-            if Int(Fleur.frame.origin.y) <= (screenHeight+100)/2 {
-                v = CGPoint(x:4, y:Int.random(in: 0...2))
-            }
-            else{
-                v = CGPoint(x:4, y:Int.random(in: -2...0))
-            }
-        }
-        else {
-            if Int(Fleur.frame.origin.y) <= screenHeight/2 {
-                v = CGPoint(x:-4, y:Int.random(in: 0...2))
-            }
-            else{
-                v = CGPoint(x:-4, y:Int.random(in: -2...0))
-            }
-        }
+    
+    func lancerSpawn() {
+        let attente = Double(Int.random(in: 1...4)) * 0.5
+        
+        t2 = Timer.scheduledTimer(timeInterval: attente,
+                                  target: self,
+                                  selector: #selector(activation),
+                                  userInfo: nil,
+                                  repeats: false)
     }
     
-    @objc func boucle(t: Timer) {
-        var p : CGPoint = Fleur.frame.origin
-        p.x += v.x
-        p.y += v.y
-        if (-100...screenWidth+100).contains(Int(p.x)) {
-        }
-        else {
-            nbVie+=1
-            t.invalidate()
-            p.x = p.x - v.x
-            p.y = p.y - v.y
-            vie1.isHidden = (nbVie<=1)
-            vie2.isHidden = (nbVie<=2)
-            vie3.isHidden = (nbVie<=3)
-            if nbVie>3 {
-                t2.invalidate()
-                finpartie.isHidden = false
-                boutonmenu.isHidden = false
-                boutonrejouer.isHidden = false
-                boutonpause.isHidden = true
+    @objc func activation(_ timer: Timer) {
+        for fleur in fleurs {
+            if fleur.isHidden == true{
+                fleur.isHidden = false
+                fleur.frame.origin = CGPoint(x: positionx[Int.random(in: 0...1)], y: Int.random(in:100...screenHeight))
+                if fleur.frame.origin.x < 0 {
+                    if Int(fleur.frame.origin.y) <= (screenHeight+100)/2 {
+                        vitesses[fleur] = CGPoint(x:4, y:Int.random(in: 0...2))
+                    }
+                    else{
+                        vitesses[fleur] = CGPoint(x:4, y:Int.random(in: -2...0))
+                    }
+                }
+                else {
+                    if Int(fleur.frame.origin.y) <= screenHeight/2 {
+                        vitesses[fleur] = CGPoint(x:-4, y:Int.random(in: 0...2))
+                    }
+                    else{
+                        vitesses[fleur] = CGPoint(x:-4, y:Int.random(in: -2...0))
+                    }
+                }
+                break
             }
         }
-        Fleur.frame.origin = p
+        lancerSpawn()
+
+    }
+
+    
+    @objc func boucle(_ timer: Timer) {
+        for fleur in fleurs {
+            if fleur.isHidden == false {
+                var p : CGPoint = fleur.frame.origin
+                if let v = vitesses[fleur] {
+                    p.x += v.x
+                    p.y += v.y
+                }
+                if (-100...screenWidth+100).contains(Int(p.x)){
+                    fleur.frame.origin = p
+                }
+                else {
+                    nbVie+=1
+                    fleur.isHidden = true
+                    fleur.frame.origin = CGPoint(x:-100, y:381)
+                    vitesses[fleur] = CGPoint(x:0, y:0)
+                    vie1.isHidden = (nbVie<1)
+                    vie2.isHidden = (nbVie<2)
+                    vie3.isHidden = (nbVie<3)
+
+                    if nbVie > 2 {
+                        t.invalidate()
+                        t2.invalidate()
+                        finpartie.isHidden = false
+                        boutonmenu.isHidden = false
+                        boutonrejouer.isHidden = false
+                        boutonpause.isHidden = true
+                    }
+                }
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         screenHeight = Int(UIScreen.main.bounds.height)-100
         screenWidth = Int(UIScreen.main.bounds.width)
-        positionx = [-50, screenWidth+50]
+        positionx = [-100, screenWidth+100]
+        fleurs = [fleur1, fleur2, fleur3, fleur4]
         
         t = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(boucle), userInfo: nil, repeats: true)
-        
-        t2 = Timer.scheduledTimer(timeInterval: 0.7, target: self, selector: #selector(deplace), userInfo: nil, repeats: true)
-
+                
+        lancerSpawn()
     }
     
 }
