29f247cf74daf876a95c3bf72a2556f37131f1be
Tom <tom.lesellier@univ-lyon2.fr>
2026-03-20 11:20:13 +0100

    push presque bombe

--- SakuraNinja/GameViewController.swift
+++ SakuraNinja/GameViewController.swift
@@ -17,11 +17,15 @@ class GameViewController: UIViewController {
     var t: Timer!
     var t2: Timer!
     var attente : Int = 0
+    var Score : Int = 0
 
     var screenHeight : Int = 0
     var screenWidth : Int = 0
     var positionx : [Int] = []
     var temps : Int = 0
+    var maxSpeed : Int = 0
+    
+    var difficulte: Int = 1
     
     @IBOutlet weak var boutonpause: UIButton!
     @IBOutlet weak var boutonmenu: UIButton!
@@ -30,13 +34,37 @@ class GameViewController: UIViewController {
 
     @IBOutlet weak var finpartie: UILabel!
     
+    @IBOutlet weak var scorelabel: UILabel!
     @IBOutlet weak var fleur1: UIImageView!
     @IBOutlet weak var fleur2: UIImageView!
     @IBOutlet weak var fleur3: UIImageView!
     @IBOutlet weak var fleur4: UIImageView!
+    @IBOutlet weak var bombe: UIImageView!
     var fleurs : [UIImageView] = []
     let fleur = UIImageView()
 
+    var dernierPoint: CGPoint?
+    var pointActuel: CGPoint?
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let t = touches.first {
+            let p = t.location(in: view)
+            dernierPoint = p
+            pointActuel = p
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let t = touches.first {
+            pointActuel = t.location(in: view)
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dernierPoint = nil
+        pointActuel = nil
+    }
+    
     
     @IBAction func rejouer(_ sender: Any) {
         nbVie=0
@@ -51,12 +79,14 @@ class GameViewController: UIViewController {
         fleur2.isHidden=true
         fleur3.isHidden=true
         fleur4.isHidden=true
+        Score = 0
+        scorelabel.text = "Score : \(Score)"
         print(fleur1.frame.origin)
         t = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(boucle), userInfo: nil, repeats: true)
         lancerSpawn()    }
     
     func lancerSpawn() {
-        let attente = Double(Int.random(in: 1...4)) * 0.2
+        let attente = Double(Int.random(in: 1...4)) * 0.2 * Double(3 - difficulte)
         
         t2 = Timer.scheduledTimer(timeInterval: attente,
                                   target: self,
@@ -66,24 +96,28 @@ class GameViewController: UIViewController {
     }
     
     @objc func activation(_ timer: Timer) {
+        let spawnBombe = Int.random(in: 1...6) == 1
+        if spawnBombe {
+            fleur = [bombe,fleur1, fleur2, fleur3, fleur4]
+        }
         for fleur in fleurs {
             if fleur.isHidden == true{
                 fleur.isHidden = false
                 fleur.frame.origin = CGPoint(x: positionx[Int.random(in: 0...1)], y: Int.random(in:100...screenHeight))
                 if fleur.frame.origin.x < 0 {
                     if Int(fleur.frame.origin.y) <= (screenHeight+100)/2 {
-                        vitesses[fleur] = CGPoint(x:4, y:Int.random(in: 0...2))
+                        vitesses[fleur] = CGPoint(x:maxSpeed, y:Int.random(in: 0...2))
                     }
                     else{
-                        vitesses[fleur] = CGPoint(x:4, y:Int.random(in: -2...0))
+                        vitesses[fleur] = CGPoint(x:maxSpeed, y:Int.random(in: -2...0))
                     }
                 }
                 else {
                     if Int(fleur.frame.origin.y) <= screenHeight/2 {
-                        vitesses[fleur] = CGPoint(x:-4, y:Int.random(in: 0...2))
+                        vitesses[fleur] = CGPoint(x:-maxSpeed, y:Int.random(in: 0...2))
                     }
                     else{
-                        vitesses[fleur] = CGPoint(x:-4, y:Int.random(in: -2...0))
+                        vitesses[fleur] = CGPoint(x:-maxSpeed, y:Int.random(in: -2...0))
                     }
                 }
                 break
@@ -93,6 +127,15 @@ class GameViewController: UIViewController {
 
     }
 
+    func couperFleur(_ fleur: UIImageView) {
+        fleur.isHidden = true
+        fleur.frame.origin = CGPoint(x: -100, y: 381)
+        vitesses[fleur] = CGPoint(x: 0, y: 0)
+        Score += 1
+        scorelabel.text = "Score : \(Score)"
+        print("Fleur coupée 🌸")
+    }
+    
     
     @objc func boucle(_ timer: Timer) {
         for fleur in fleurs {
@@ -130,15 +173,26 @@ class GameViewController: UIViewController {
                 }
             }
         }
+        if let p1 = dernierPoint, let p2 = pointActuel {
+            
+            for fleur in fleurs {
+                if fleur.isHidden == false {
+                    
+                    if fleur.frame.contains(p1) || fleur.frame.contains(p2) {
+                        couperFleur(fleur)
+                    }
+                }
+            }
+            dernierPoint = p2
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         screenHeight = Int(UIScreen.main.bounds.height)-100
         screenWidth = Int(UIScreen.main.bounds.width)
         positionx = [-100, screenWidth+100]
         fleurs = [fleur1, fleur2, fleur3, fleur4]
-        
+        maxSpeed = (difficulte + 2)
         t = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(boucle), userInfo: nil, repeats: true)
                 
         lancerSpawn()
--- SakuraNinja/ViewController.swift
+++ SakuraNinja/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var segmentDifficulte: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? GameViewController {
+            destination.difficulte = segmentDifficulte.selectedSegmentIndex
+        }
     }
 
 
